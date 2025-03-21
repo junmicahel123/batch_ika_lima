e61f89f60b44fe46f5621d79c22a60c69558109d
Owen <juanmig134187@gmail.com>
2025-03-21 14:38:15 +0800

    character_counter_

---
+++ chharacter_counter_5.8.py
@@ -0,0 +1,13 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+#Example:
+#Input: Juan Dela Cruz
+#utput: 14
+
+# ask user for input 
+# count the len of characters
+# print len
+
+statement_ = input("Input your full name:: ")
+
+counter_ = len(statement_)
+print(counter_)
