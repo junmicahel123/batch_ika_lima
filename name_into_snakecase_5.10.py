4f3c8f8c31d264b79442d259f3831b2b0dd216c7
Owen <juanmig134187@gmail.com>
2025-03-21 14:47:17 +0800

    done_converter

---
+++ name_into_snakecase_5.10.py
@@ -0,0 +1,15 @@
+#Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: juan_dela_cruz
+
+
+# ask user for their fullname
+# convert using lower.replace
+# print 
+
+fullname = input("What's your full name?:")
+
+coverter = fullname.lower().replace(" ", "_")
+
+print(coverter)
