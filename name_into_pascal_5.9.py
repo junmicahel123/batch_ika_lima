841d6afeed51485b423974c192f0a4eeafdbaca0
Owen <juanmig134187@gmail.com>
2025-03-21 14:41:02 +0800

    pseudocoererts

---
+++ name_into_pascal_5.9.py
@@ -0,0 +1,9 @@
+#Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuanDelaCruz
+
+# ask user for input
+# convert using title.replace
+# print
+
