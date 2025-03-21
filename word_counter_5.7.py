700d25b18fd4674bae8bfc1153a3ff6afab43a80
Owen <juanmig134187@gmail.com>
2025-03-21 14:22:55 +0800

    pseduocode_

---
+++ word_counter_5.7.py
@@ -0,0 +1,9 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+#Example:
+#Input: We will weather the weather whatever the weather whether we like it or not
+#Output: 14
+
+# ask user for input 
+# convert iusing len and split()
+# print 
+
