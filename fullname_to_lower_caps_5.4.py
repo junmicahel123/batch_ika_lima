9f58a16fb67be4cc16493fc740384511919abdc1
Owen <juanmig134187@gmail.com>
2025-03-21 14:06:54 +0800

    doneee__

---
+++ fullname_to_lower_caps_5.4.py
@@ -0,0 +1,14 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+#Example:
+#Input: Juan Dela Cruz
+#Output: juan dela cruz
+
+# ask user to input name
+# convert name
+# print name
+
+full_name = input("What's your name?:: ")
+
+lowered_name = full_name.lower()
+
+print(lowered_name)
