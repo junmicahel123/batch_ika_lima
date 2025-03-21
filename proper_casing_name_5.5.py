2ae97224cd1413cd076ef6b4b7fd72fd7041b54e
Owen <juanmig134187@gmail.com>
2025-03-21 14:10:08 +0800

    pseudocodeest_

---
+++ proper_casing_name_5.5.py
@@ -0,0 +1,9 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: Juan Dela Cruz
+
+#ask user for input
+# convert the input to title()
+# print
+
