4818432e669ed29310e12868b746ecd4a17406be
Owen <juanmig134187@gmail.com>
2025-03-21 14:16:24 +0800

    psedocode_

---
+++ reversed_casing_name_5.6.py
@@ -0,0 +1,9 @@
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuaN deLA cRuz
+
+# ask user for incorrect input name case 
+# convert using swapcase()
+# print product
+
